b0988597280c01bc152c0d30cc7620e61f5da2ff
Brandon Cox <brandon.cox.519@gmail.com>
2014-09-08 01:13:32 -0400

    how to build a request

--- lib/wmata/client/request.rb
+++ lib/wmata/client/request.rb
@@ -1,5 +1,33 @@
+require 'rack'
+
 module Wmata
   class Request
+    
+    @@context = 'http://api.wmata.com'
+    
+    @@services = {
+      :incidents => 'Incidents.svc',
+      :bus => 'Bus.svc',
+      :next_bus => 'NextBusService.svc',
+      :rail => 'Rail.svc',
+      :station_prediction => 'StationPrediction.svc'
+    }
+    
+    def initialize(service, path, parameters)
+      @service = @@services[service]
+      @path = path
+      @parameters = parameterize(parameters)
+    end
+    
+    def url
+      "#{@@context}/#{@service}/json/#{parameterize(@parameters)}"
+    end
+    
+    private
+    
+      def parameterize(args={})
+        Rack::Utils.build_query(args)
+      end
     
   end
 end
