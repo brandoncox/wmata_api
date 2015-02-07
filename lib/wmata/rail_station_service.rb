149598dca58d4af728192a3ea323f3721e2c3c68
Brandon Cox <bcox@redhat.com>
2015-02-07 00:49:50 -0500

    renamed file

---
+++ lib/wmata/rail_station_service.rb
@@ -0,0 +1,40 @@
+module Wmata
+  class RailStationService
+    class << self
+
+      def lines 
+        #https://api.wmata.com/Rail.svc/json/jLines?api_key=<Your subscription key>
+      end
+
+      def parking_information
+
+      end
+
+      def path_between_stations
+
+      end
+
+      def station_entrances
+
+      end
+
+      def station_information
+
+      end
+
+      def station_list
+
+      end
+
+      def station_timings
+
+      end
+
+      def stations_to_station_information
+
+      end
+
+
+    end
+  end
+end
